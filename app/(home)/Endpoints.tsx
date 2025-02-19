961d28d971c05f004aff0e38f86d19b38a0904b4
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-02-19 10:38:12 +0200

    Add real data and enhance the component UI.

--- app/(home)/Endpoints.tsx
+++ app/(home)/Endpoints.tsx
@@ -4,42 +4,46 @@ import { useState } from "react";
 
 type Endpoint = {
     endpoint: string;
-    category: 'albums' | 'quotes' | 'skyscrapers';
-    objectLoad: 'album' | 'quote' | 'skyscraper';
+    category: 'albums' | 'quotes' | 'software-tools';
+    objectLoad: 'album' | 'quote' | 'software-tool';
 };
 
 const album = JSON.stringify({
     id: 1,
-    title: 'Title',
-    description: 'Lorem ipsum dolor sit, amet elit. Natus, velit!',
-    nested: {
-        option1: 'option1',
-        option2: 'option2'
+    name: 'Thriller',
+    artist: 'Michael Jackson',
+    releaseDate: {
+        day: 29,
+        month: 'November',
+        year: 1982
     },
-    key: 'Lorem ipsum dolor sit isicing elit. Tempora, iusto.'
-}, null, 2);
+    genre: 'Pop',
+    label: 'Epic',
+    wikipedia: 'https://en.wikipedia.org/wiki/Thriller_(album)',
+}, null, 3);
 
 const quote = JSON.stringify({
-    id: 2,
-    title: 'Title',
-    description: 'Lorem ipsum dolor sit, amet elit. Natus, velit!',
-    nested: {
-        option1: 'option1',
-        option2: 'option2'
-    },
-    key: 'Lorem ipsum dolor sit isicing elit. Tempora, iusto.'
-}, null, 2);
+    id: 1,
+    quote: `When done well, software is invisible`,
+    author: `Bjarne Stroustrup`,
+    category: `Software Industry`
+}, null, 3);
 
-const skyscraper = JSON.stringify({
-    id: 3,
-    title: 'Title',
-    description: 'Lorem ipsum dolor sit, amet elit. Natus, velit!',
-    nested: {
-        option1: 'option1',
-        option2: 'option2'
-    },
-    key: 'Lorem ipsum dolor sit isicing elit. Tempora, iusto.'
-}, null, 2);
+const softwareTool = JSON.stringify({
+    id: 1,
+    title: 'C',
+    description: 'A powerful, low-level programming language known for its efficiency and direct memory manipulation',
+    usage: 'System programming, embedded systems, and performance-critical applications',
+    creators: [
+        {
+            name: 'Dennis Ritchie',
+            wiki: 'https://en.wikipedia.org/wiki/Dennis_Ritchie'
+        },
+    ],
+    yearCreated: 1972,
+    wiki: 'https://en.wikipedia.org/wiki/C_(programming_language)',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/1200px-C_Programming_Language.svg.png'
+}, null, 3);
 
 const endpoints: Endpoint[] = [
     {
@@ -53,9 +57,9 @@ const endpoints: Endpoint[] = [
         objectLoad: 'quote'
     },
     {
-        endpoint: 'timeloopsapi.com/skyscrapers/1',
-        category: 'skyscrapers',
-        objectLoad: 'skyscraper'
+        endpoint: 'timeloopsapi.com/software-tools/1',
+        category: 'software-tools',
+        objectLoad: 'software-tool'
     }
 ];
 
@@ -87,22 +91,24 @@ const Endpoints = () => {
     };
 
     return (
-        <section className="flex flex-col lg:flex-row justify-center gap-10 mb-16">
+        <section className="flex flex-col lg:flex-row justify-center gap-10 mb-14">
             <div className="flex flex-col items-center gap-3">
-                <h1 className="text-4xl font-bold mb-5 text-center">
+                <h1 className="text-4xl font-semibold mb-5 text-center">
                     Try any endpoint in your browser
                 </h1>
                 {
                     endpoints.map(endpoint => 
                         <div 
                             key={endpoint.category}
-                            data-tip={`Click to copy ${endpoint.objectLoad}!`}
+                            data-tip='Click to copy!'
                             className="tooltip tooltip-left min-w-full sm:min-w-fit"
                         >
                             <button 
-                                onClick={() => onButtonClick(endpoint.category, endpoint.objectLoad, endpoint.endpoint)}
-                                className={`min-w-full sm:min-w-96 btn cursor-copy font-mono font-light hover:border-primary
-                                    ${endpoint.objectLoad === object ? 'border border-primary shadow-inner' : ''}`}
+                                onClick={_ => onButtonClick(endpoint.category, endpoint.objectLoad, endpoint.endpoint)}
+                                className={`
+                                    btn cursor-copy font-mono font-light min-w-full sm:min-w-96 hover:border-primary
+                                    ${endpoint.objectLoad === object ? 'border border-primary shadow-inner' : ''}
+                                `}
                             >
                                 <code className="text-sm sm:text-base">{endpoint.endpoint}</code>
                             </button>
@@ -110,16 +116,18 @@ const Endpoints = () => {
                     )
                 }
             </div>
-            <div className="mockup-browser border-base-300 border max-w-auto lg:max-w-2xl">
+
+            <div className="mockup-browser mockup-browser-home bg-base-200/50 border-base-300 border xl:max-w-96">
                 <div className="mockup-browser-toolbar">
-                    <div className="input py-px border-base-300 border">https://timeloopsapi.com/
+                    <div className="input py-px border-base-300 border">
+                        https://timeloopsapi.com/
                         <span className='hidden sm:inline mx-1 bg-white/10 py-px px-1 rounded-sm border border-primary border-opacity-70'>{url}</span>
                         <span className='inline sm:hidden'>{url}</span>
                         /1
                     </div>
                 </div>
-                <div className="border-base-300 flex justify-start border-t px-4 py-5">
-                    <pre>{object === 'album' ? album : object === 'quote' ? quote : skyscraper}</pre>
+                <div className='border-base-300 flex justify-start border-t px-4 py-6'>
+                    <pre>{object === 'album' ? album : object === 'quote' ? quote : softwareTool}</pre>
                 </div>
             </div>
         </section>
