9d5505d84355550df32d03c2d88c13b503789881
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-03-02 13:04:36 +0200

    add month schema

---
+++ app/(api)/monthSchema.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const monthSchema = z.enum([
+  'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+]);
