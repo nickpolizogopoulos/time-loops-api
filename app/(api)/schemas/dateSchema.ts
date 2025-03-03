69dd12459267afbd79a4b95acc916ad186e77c16
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-03-03 20:47:17 +0200

    Put date, month schemas in a "schemas" folder and add utility schemas.

---
+++ app/(api)/schemas/dateSchema.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+import { monthSchema } from './monthSchema';
+
+export const dateSchema = z.object({
+    day: z.number()
+      .int('Day must be an integer')
+      .min(1, 'Day must be at least 1')
+      .max(31, 'Day cannot be more than 31'),
+    month: monthSchema,
+    year: z.number()
+      .int('Year must be an integer')
+      .min(1900, 'Year must be at least 1900')
+      .max(new Date().getFullYear(), 'Year cannot be in the future'),
+  });
---
+++ app/(api)/schemas/utilitySchemas.ts
@@ -0,0 +1,7 @@
+import { z } from 'zod';
+
+export const URL = z.string().url({ message: 'Invalid URL format' });
+
+export const positiveNumber = z.number()
+    .int()
+    .positive({ message: 'Please enter a number greater than 0' });
