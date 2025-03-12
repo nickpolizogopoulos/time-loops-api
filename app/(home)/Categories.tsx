2c002f86e9cbeb54f185fba3f555f57394ea59ed
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-03-12 20:52:05 +0200

    Remove react fragment from Categories component.

--- app/(home)/Categories.tsx
+++ app/(home)/Categories.tsx
@@ -44,46 +44,44 @@ const cards: Card[] = [
 
 const Categories = () => {
     return (
-        <>
-            <section className="mb-12 md:mb-10">
-                <h2 className="text-4xl font-semibold text-center mb-10">
-                    Choose between four categories
-                </h2>
-                <div className="flex flex-wrap justify-center gap-6">
-                    {
-                        cards.map((card) => (
-                            <div
-                                key={card.description}
-                                className="card bg-base-200/50 shadow-lg card-bordered border-primary max-w-96 sm:w-[48%] lg:w-96"
-                            >
-                                <div className="card-body">
-                                    <h2 className="card-title font-semibold text-3xl">
-                                        {card.title}
-                                    </h2>
-                                    <p>{card.description}</p>
-                                    <div className="divider" />
-                                    <div className="card-actions justify-between">
-                                        <Link
-                                            href={`/docs#${card.docksPath}`}
-                                            className="btn btn-primary btn-outline bg-base-100 min-w-36"
-                                        >
-                                            See on Docs
-                                        </Link>
-                                        <Link
-                                            target='_blank'
-                                            href={`${card.buttonText}`}
-                                            className="btn btn-primary min-w-36"
-                                        >
-                                            {card.buttonText}
-                                        </Link>
-                                    </div>
+        <section className="mb-12 md:mb-10">
+            <h2 className="text-4xl font-semibold text-center mb-10">
+                Choose between four categories
+            </h2>
+            <div className="flex flex-wrap justify-center gap-6">
+                {
+                    cards.map((card) => (
+                        <div
+                            key={card.description}
+                            className="card bg-base-200/50 shadow-lg card-bordered border-primary max-w-96 sm:w-[48%] lg:w-96"
+                        >
+                            <div className="card-body">
+                                <h2 className="card-title font-semibold text-3xl">
+                                    {card.title}
+                                </h2>
+                                <p>{card.description}</p>
+                                <div className="divider" />
+                                <div className="card-actions justify-between">
+                                    <Link
+                                        href={`/docs#${card.docksPath}`}
+                                        className="btn btn-primary btn-outline bg-base-100 min-w-36"
+                                    >
+                                        See on Docs
+                                    </Link>
+                                    <Link
+                                        target='_blank'
+                                        href={`${card.buttonText}`}
+                                        className="btn btn-primary min-w-36"
+                                    >
+                                        {card.buttonText}
+                                    </Link>
                                 </div>
                             </div>
-                        ))
-                    }
-                </div>
-            </section>
-        </>
+                        </div>
+                    ))
+                }
+            </div>
+        </section>
     );
 };
 
