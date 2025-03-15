ea010384f651978a6526b8abda16c8fbbaca5a81
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-03-15 13:13:52 +0200

    Add ProjectInformation component to Home page layout

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,15 @@
+import HeroSection from "./HeroSection";
+import Endpoints from "./Endpoints";
+import ProjectInformation from "./ProjectInformation";
 import Categories from "./Categories";
 import CTA from "./CTA";
-import Endpoints from "./Endpoints";
-import HeroSection from "./HeroSection";
 
 const Home = () => {
   return (
     <main className='flex flex-col gap gap-0 sm:gap-10 px-5'>
       <HeroSection />
       <Endpoints />
+      <ProjectInformation />
       <Categories />
       <CTA />
     </main>
