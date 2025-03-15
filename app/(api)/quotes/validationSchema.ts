e742bf0dfd2aae67785f9f32076c1c8c71038c0c
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-03-15 14:06:35 +0200

    Update the quote validationSchema

--- app/(api)/quotes/validationSchema.ts
+++ app/(api)/quotes/validationSchema.ts
@@ -3,9 +3,25 @@ import { z } from 'zod';
 export const quoteSchema = z.object({
     quote: z.string().min(4, { message: 'Quote must be at least 4 characters long.' }),
     author: z.string().min(1, { message: 'Author is required.' }),
-    category: z.enum([
-        'software-industry',
-        'general',
-        'creativity'
-    ], { message: `Category must be either software-industry, general or creativity` })
+    category: z.enum(['technology',
+        'wisdom',
+        'education',
+        'motivation',
+        'success',
+        'creativity',
+        'philosophy',
+        'strategy',
+        'relationships',
+        'life',
+        'self-improvement',
+        'gaming',
+        'tv-series',
+        'leadership',
+        'psychology',
+        'personal-growth',
+        'love',
+        'strength',
+        'self-awareness',
+        'ambition'
+    ], { message: 'Category must be one of the following: technology, wisdom, education, motivation, success, creativity, philosophy, strategy, relationships, life, self-improvement, gaming, tv-series, leadership, psychology, personal-growth, love, strength, self-awareness, ambition.'})
 });
