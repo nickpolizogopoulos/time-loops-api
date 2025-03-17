c2a14120d9d2c22cc8df8020e153664fe9c9ccc5
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-03-18 00:32:46 +0200

    Implement PUT, PATCH, and OPTIONS methods for software tool management

--- app/(api)/software-tools/[id]/route.ts
+++ app/(api)/software-tools/[id]/route.ts
@@ -5,6 +5,7 @@ import {
 
 import { getHeaders } from '../../headers';
 import softwareTools from '../data.json';
+import { softwareToolSchema } from '../validationSchema';
 
 
 export const GET = async (request: NextRequest) => {
@@ -35,4 +36,70 @@ export const DELETE = async (request: NextRequest) => {
     { message: `The Software Tool with id ${id} (${tool.title}), has been deleted successfully.` },
     { status: 200, headers }
   );
+};
+
+
+export const PUT = async (request: NextRequest) => {
+  const url = new URL(request.url);
+  const headers = getHeaders('PUT');
+  const id = parseInt(url.pathname.split('/').pop()!);
+  const toolIndex = softwareTools.findIndex(tool => tool.id === id);
+
+  if (toolIndex === -1) 
+    return NextResponse.json({ message: 'Software tool not found' }, { status: 404, headers });
+
+  const body = await request.json();
+  const validation = softwareToolSchema.safeParse(body);
+  
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400, headers });
+    
+  softwareTools[toolIndex] = { ...softwareTools[toolIndex], ...body };
+
+  return NextResponse.json(
+    { 
+      message: `The Software Tool with id ${id} (${validation.data.title}), has been updated successfully.`,
+      updatedQuote: softwareTools[toolIndex]
+    },
+    { status: 200, headers }
+  );
+};
+
+
+export const PATCH = async (request: NextRequest) => {
+  const url = new URL(request.url);
+  const headers = getHeaders('PATCH');
+  const id = parseInt(url.pathname.split('/').pop()!);
+  const toolIndex = softwareTools.findIndex(tool => tool.id === id);
+
+  if (toolIndex === -1) 
+    return NextResponse.json({ message: 'Software tool not found' }, { status: 404, headers });
+
+  const body = await request.json();
+  const validation = softwareToolSchema.partial().safeParse(body);
+  
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400, headers });
+
+  softwareTools[toolIndex] = { ...softwareTools[toolIndex], ...body };
+
+  return NextResponse.json(
+    { 
+      message: `The Software Tool with id ${id} has been partially updated.`,
+      updatedQuote: softwareTools[toolIndex]
+    },
+    { status: 200, headers }
+  );
+};
+
+
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  });
 };
