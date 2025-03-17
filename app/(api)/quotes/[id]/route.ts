e7f575262725aaf1903bd8ed45d6f9081aafb75e
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-03-18 00:32:36 +0200

    Implement PUT, PATCH, and OPTIONS methods for quote management

--- app/(api)/quotes/[id]/route.ts
+++ app/(api)/quotes/[id]/route.ts
@@ -5,6 +5,8 @@ import {
 
 import { getHeaders } from '../../headers';
 import quotes from '../data.json';
+import { quoteSchema } from '../validationSchema';
+
 
 export const GET = async (request: NextRequest) => {
   const url = new URL(request.url);
@@ -34,4 +36,70 @@ export const DELETE = async (request: NextRequest) => {
     { message: `The Quote with id ${id} (by ${quote.author}) has been deleted successfully.` },
     { status: 200, headers }
   );
+};
+
+
+export const PUT = async (request: NextRequest) => {
+  const url = new URL(request.url);
+  const headers = getHeaders('PUT');
+  const id = parseInt(url.pathname.split('/').pop()!);
+  const quoteIndex = quotes.findIndex(quote => quote.id === id);
+
+  if (quoteIndex === -1) 
+    return NextResponse.json({ message: 'Quote not found' }, { status: 404, headers });
+
+  const body = await request.json();
+  const validation = quoteSchema.safeParse(body);
+  
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400, headers });
+    
+  quotes[quoteIndex] = { ...quotes[quoteIndex], ...body };
+
+  return NextResponse.json(
+    { 
+      message: `The Quote with id ${id} (by ${validation.data.author}) has been updated successfully.`,
+      updatedQuote: quotes[quoteIndex]
+    },
+    { status: 200, headers }
+  );
+};
+
+
+export const PATCH = async (request: NextRequest) => {
+  const url = new URL(request.url);
+  const headers = getHeaders('PATCH');
+  const id = parseInt(url.pathname.split('/').pop()!);
+  const quoteIndex = quotes.findIndex(quote => quote.id === id);
+
+  if (quoteIndex === -1) 
+    return NextResponse.json({ message: 'Quote not found' }, { status: 404, headers });
+
+  const body = await request.json();
+  const validation = quoteSchema.partial().safeParse(body);
+  
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400, headers });
+
+  quotes[quoteIndex] = { ...quotes[quoteIndex], ...body };
+
+  return NextResponse.json(
+    { 
+      message: `The Quote with id ${id} has been partially updated successfully.`,
+      updatedQuote: quotes[quoteIndex]
+    },
+    { status: 200, headers }
+  );
+};
+
+
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  });
 };
