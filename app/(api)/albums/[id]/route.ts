0ab8d1e239ee3df3aef42676efab81009194b7f9
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-03-18 00:32:14 +0200

    Implement PUT and PATCH methods for album updates and add OPTIONS support

--- app/(api)/albums/[id]/route.ts
+++ app/(api)/albums/[id]/route.ts
@@ -5,6 +5,7 @@ import {
 
 import { getHeaders } from '../../headers';
 import musicAlbums from '../data.json';
+import { musicAlbumSchema } from '../validationSchema';
 
 
 export const GET = async (request: NextRequest) => {
@@ -35,4 +36,70 @@ export const DELETE = async (request: NextRequest) => {
     { message: `The Album with id ${id} (${album.title} by ${album.artist}), has been deleted successfully.` },
     { status: 200, headers }
   );
+};
+
+
+export const PUT = async (request: NextRequest) => {
+  const url = new URL(request.url);
+  const headers = getHeaders('PUT');
+  const id = parseInt(url.pathname.split('/').pop()!);
+  const albumIndex = musicAlbums.findIndex(album => album.id === id);
+
+  if (albumIndex === -1) 
+    return NextResponse.json({ message: 'Album not found' }, { status: 404, headers });
+
+  const body = await request.json();
+  const validation = musicAlbumSchema.safeParse(body);
+  
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400, headers });
+    
+  musicAlbums[albumIndex] = { ...musicAlbums[albumIndex], ...body };
+
+  return NextResponse.json(
+    { 
+      message: `The Album with id ${id} (${validation.data.title} by ${validation.data.artist}), has been updated successfully.`,
+      updatedQuote: musicAlbums[albumIndex]
+    },
+    { status: 200, headers }
+  );
+};
+
+
+export const PATCH = async (request: NextRequest) => {
+  const url = new URL(request.url);
+  const headers = getHeaders('PATCH');
+  const id = parseInt(url.pathname.split('/').pop()!);
+  const albumIndex = musicAlbums.findIndex(album => album.id === id);
+
+  if (albumIndex === -1) 
+    return NextResponse.json({ message: 'Album not found' }, { status: 404, headers });
+
+  const body = await request.json();
+  const validation = musicAlbumSchema.partial().safeParse(body);
+  
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400, headers });
+
+  musicAlbums[albumIndex] = { ...musicAlbums[albumIndex], ...body };
+
+  return NextResponse.json(
+    { 
+      message: `The Album with id ${id} has been partially updated.`,
+      updatedQuote: musicAlbums[albumIndex]
+    },
+    { status: 200, headers }
+  );
+};
+
+
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  });
 };
