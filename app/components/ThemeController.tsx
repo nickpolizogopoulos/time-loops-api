e78058c8866eb07870f068be79747d4940d938de
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-03-20 10:46:11 +0200

    Remove unused theme variable from useState./

--- app/components/ThemeController.tsx
+++ app/components/ThemeController.tsx
@@ -7,7 +7,7 @@ import {
 } from "react";
 
 const ThemeController = () => {
-    const { theme, setTheme, resolvedTheme } = useTheme();
+    const { setTheme, resolvedTheme } = useTheme();
     const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
